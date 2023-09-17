6be2ed2ae950ec070b02dc7ab4c4c75e87bfd96c
LuizRomanhol <48190640+LuizRomanhol@users.noreply.github.com>
2023-09-17 20:49:19 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+posts = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        post = request.form['post']
+        posts.append(post)
+    return render_template('index.html', posts=posts[-5:])
+
+if __name__ == '__main__':
+    app.run()
+
