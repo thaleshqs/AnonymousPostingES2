f6096d034dfe8657153eac07c28d3dfde76ae817
LuizRomanhol <48190640+LuizRomanhol@users.noreply.github.com>
2023-09-17 21:07:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ def index():
     if request.method == 'POST':
         post = request.form['post']
         posts.append(post)
-    return render_template('index.html', posts=posts[-5:])
+    return render_template('index.html', posts=reversed(posts[-5:]))
 
 if __name__ == '__main__':
     app.run()
-
