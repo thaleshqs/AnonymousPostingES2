1f98a9bf058f68c3d6e0004cdb130edd83f2a502
LuizRomanhol <48190640+LuizRomanhol@users.noreply.github.com>
2023-10-29 19:31:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,66 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from datetime import datetime
 
 app = Flask(__name__)
 
-posts = []
+messages = []
+
+def get_next_id():
+    return len(messages)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    filter_tag = request.args.get('filter')
+    if filter_tag is None:
+        filter_tag = 'all'
+
     if request.method == 'POST':
+        name = request.form['name']
         post = request.form['post']
-        posts.append(post)
-    return render_template('index.html', posts=reversed(posts[-5:]))
+        
+        categories = request.form.getlist('categories')
+        comment = request.form.get('comment')
+        parent_id = request.form.get('parent_id')
+        selected_categories = request.form.getlist('categories')
+
+        if not post.strip() and (comment is None or not comment.strip()):
+            return render_template('index.html', messages=messages, error="Message or comment cannot be blank.", categories=["Secrets", "Family", "Health", "Confession", "Other"], filter=filter_tag)
+
+        if len(selected_categories) > 2:
+            return render_template('index.html', messages=messages, categories=["Secrets", "Family", "Health", "Confession", "Other"], error="You can select up to 2 categories.", filter=filter_tag)
+
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        if comment:  
+            filter_tag = request.form['filter']
+            comment_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            comment_data = {
+                'name': name,
+                'comment': comment,
+                'timestamp': comment_timestamp,
+                'categories': categories
+            }
+
+            if parent_id is not None:
+                parent_id = int(parent_id)
+                messages[parent_id]['comments'].append(comment_data)
+            return redirect(f'/?filter={filter_tag}')
+        else:
+            message = {
+                'id': get_next_id(),
+                'name': name,
+                'post': post,
+                'timestamp': timestamp,
+                'categories': categories,
+                'comments': []
+            }
+            messages.append(message)
+
+        
+
+    filtered_messages = [message for message in messages if filter_tag is None or filter_tag == 'all' or filter_tag in message['categories']]
+
+    return render_template('index.html', messages=filtered_messages, error=None, categories=["Secrets", "Family", "Health", "Confession", "Other"], filter=filter_tag)
 
 if __name__ == '__main__':
     app.run()
