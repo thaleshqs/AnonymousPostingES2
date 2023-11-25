f56db6b3b90f81b704e1dd9f3016a3915ea1904c
thaleshqs <59453636+thaleshqs@users.noreply.github.com>
2023-11-24 21:34:08 -0300

    Update selenium_tests.py

--- tests/selenium_tests.py
+++ tests/selenium_tests.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
@@ -9,65 +10,71 @@ import time
 import unittest
 
 class SystemTest(unittest.TestCase):
-  def setUp(self):
-    display = Display(visible=0, size=(800, 800))  
-    display.start()
-    
-    chromedriver_autoinstaller.install() 
-    chrome_options = webdriver.ChromeOptions()    
-    options = [
-       "--window-size=1200,1200",
-        "--ignore-certificate-errors"
-     ]
-    for option in options:
-        chrome_options.add_argument(option)
-    self.app_url = 'http://127.0.0.1:5000'
-    self.driver = webdriver.Chrome(options = chrome_options)
-    
-  def tearDown(self):
-    self.driver.quit()
+    def setUp(self):
+        display = Display(visible=0, size=(800, 800))
+        display.start()
 
-  def test_write_post_and_comment(self):
-    try:
-      self.driver.get(self.app_url)
-    
-      name_input = self.driver.find_element('name', 'name')
-      post_input = self.driver.find_element('name', 'post')
-      #comment_input = self.driver.find_element('name', 'comment')
-      comment_input = self.driver.find_element(By.CSS_SELECTOR, 'input[name="comment"]')
-  
-      name_input.send_keys('John Doe')
-      post_input.send_keys('This is a test post.')
-      comment_input.send_keys('This is a test comment.')
-  
-      post_input.send_keys(Keys.RETURN)
-  
-      time.sleep(2)
-      assert 'This is a test post' in self.driver.page_source
-      assert 'This is a test comment.' in self.driver.page_source
+        chromedriver_autoinstaller.install()
+        chrome_options = webdriver.ChromeOptions()
+        options = [
+            "--window-size=1200,1200",
+            "--ignore-certificate-errors"
+        ]
+        for option in options:
+            chrome_options.add_argument(option)
+        self.app_url = 'http://127.0.0.1:5000'
+        self.driver = webdriver.Chrome(options=chrome_options)
 
-  def test_write_post_with_category_and_use_filter(self):
-    try:
-      self.driver.get(app_url)
-    
-      name_input = self.driver.find_element('name', 'name')
-      post_input = self.driver.find_element('name', 'post')
-      categories_checkbox = self.driver.find_element(By.XPATH, "//input[@value='Secrets']")
-      submit_button = self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-    
-      name_input.send_keys('John Doe')
-      post_input.send_keys('This is a secret post.')
-      categories_checkbox.click()
-      submit_button.click()
-    
-      time.sleep(2)
-    
-      secrets_filter_button = self.driver.find_element(By.CSS_SELECTOR, '"button[value='Secrets']")
-      secrets_filter_button.click()
-    
-      time.sleep(2)
-      assert 'This is a secret post.' in self.driver.page_source
-  
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_write_post_and_comment(self):
+        try:
+            self.driver.get(self.app_url)
+
+            name_input = self.driver.find_element('name', 'name')
+            post_input = self.driver.find_element('name', 'post')
+            comment_input = self.driver.find_element(By.CSS_SELECTOR, 'input[name="comment"]')
+
+            name_input.send_keys('John Doe')
+            post_input.send_keys('This is a test post.')
+            comment_input.send_keys('This is a test comment.')
+
+            post_input.send_keys(Keys.RETURN)
+
+            time.sleep(2)
+            assert 'This is a test post' in self.driver.page_source
+            assert 'This is a test comment.' in self.driver.page_source
+
+        except Exception as e:
+            print(f"Exception in test_write_post_and_comment: {e}")
+            raise
+
+    def test_write_post_with_category_and_use_filter(self):
+        try:
+            self.driver.get(self.app_url)
+
+            name_input = self.driver.find_element('name', 'name')
+            post_input = self.driver.find_element('name', 'post')
+            categories_checkbox = self.driver.find_element(By.XPATH, "//input[@value='Secrets']")
+            submit_button = self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+
+            name_input.send_keys('John Doe')
+            post_input.send_keys('This is a secret post.')
+            categories_checkbox.click()
+            submit_button.click()
+
+            time.sleep(2)
+
+            secrets_filter_button = self.driver.find_element(By.CSS_SELECTOR, "button[value='Secrets']")
+            secrets_filter_button.click()
+
+            time.sleep(2)
+            assert 'This is a secret post.' in self.driver.page_source
+
+        except Exception as e:
+            print(f"Exception in test_write_post_with_category_and_use_filter: {e}")
+            raise
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
