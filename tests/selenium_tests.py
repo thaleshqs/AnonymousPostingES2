9cdd0301bcdd4eab0224b0983373ccbec85935e4
thaleshqs <59453636+thaleshqs@users.noreply.github.com>
2023-12-04 10:48:25 -0300

    Update selenium_tests.py

--- tests/selenium_tests.py
+++ tests/selenium_tests.py
@@ -48,116 +48,116 @@ class SystemTest(unittest.TestCase):
             print(f"Exception in test_write_post: {e}")
             raise
     
-def test_write_empty_post(self):
-    try:
-        self.driver.get(self.app_url)
-
-        name_input = self.driver.find_element(By.NAME, 'name')
-        post_input = self.driver.find_element(By.NAME, 'post')
-
-        name_input.send_keys('John Doe')
-        post_input.send_keys(Keys.RETURN)
-
-        WebDriverWait(self.driver, 10).until(
-            EC.text_to_be_present_in_element((By.XPATH, "//div[@class='alert alert-danger']"), 'Message or comment cannot be blank.')
-        )
-        assert 'Message or comment cannot be blank.' in self.driver.page_source
-
-    except Exception as e:
-        print(f"Exception in test_write_empty_post: {e}")
-        raise
-
-def test_write_post_and_comment(self):
-    try:
-        self.driver.get(self.app_url)
-
-        name_input = self.driver.find_element(By.NAME, 'name')
-        post_input = self.driver.find_element(By.NAME, 'post')
-
-        name_input.send_keys('John Doe')
-        post_input.send_keys('This is a test post.')
-        post_input.send_keys(Keys.RETURN)
-
-        WebDriverWait(self.driver, 10).until(
-            EC.text_to_be_present_in_element((By.XPATH, "//div[@class='card-body']/blockquote/p"), 'This is a test post.')
-        )
-
-        comment_input = self.driver.find_element(By.CSS_SELECTOR, 'input[name="comment"]')
-        comment_input.send_keys('This is a test comment.')
-        comment_input.send_keys(Keys.RETURN)
-
-        WebDriverWait(self.driver, 10).until(
-            EC.text_to_be_present_in_element((By.XPATH, "//div[@class='card-body']/blockquote/p"), 'This is a test comment.')
-        )
-
-        assert 'This is a test post' in self.driver.page_source
-        assert 'This is a test comment.' in self.driver.page_source
-
-    except Exception as e:
-        print(f"Exception in test_write_post_and_comment: {e}")
-        raise
-
-def test_write_post_with_category_and_use_filter(self):
-    try:
-        self.driver.get(self.app_url)
-
-        name_input = self.driver.find_element(By.NAME, 'name')
-        post_input = self.driver.find_element(By.NAME, 'post')
-        categories_checkbox = self.driver.find_element(By.XPATH, "//input[@value='Secrets']")
-        submit_button = self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-
-        name_input.send_keys('John Doe')
-        post_input.send_keys('This is a secret post.')
-        categories_checkbox.click()
-        submit_button.click()
-
-        WebDriverWait(self.driver, 10).until(
-            EC.text_to_be_present_in_element((By.XPATH, "//div[@class='card-body']/blockquote/p"), 'This is a secret post.')
-        )
-
-        secrets_filter_button = self.driver.find_element(By.CSS_SELECTOR, "button[value='Secrets']")
-        secrets_filter_button.click()
-
-        WebDriverWait(self.driver, 10).until(
-            EC.text_to_be_present_in_element((By.XPATH, "//div[@class='card-body']/blockquote/p"), 'This is a secret post.')
-        )
-        assert 'This is a secret post.' in self.driver.page_source
-
-    except Exception as e:
-        print(f"Exception in test_write_post_with_category_and_use_filter: {e}")
-        raise
-
-def test_write_post_and_search(self):
-    try:
-        self.driver.get(self.app_url)
-
-        name_input = self.driver.find_element(By.NAME, 'name')
-        post_input = self.driver.find_element(By.NAME, 'post')
-        submit_button = self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-
-        name_input.send_keys('John Doe')
-        post_input.send_keys('This is a test post.')
-        submit_button.click()
-
-        WebDriverWait(self.driver, 10).until(
-            EC.text_to_be_present_in_element((By.XPATH, "//div[@class='card-body']/blockquote/p"), 'This is a test post.')
-        )
-
-        search_input = self.driver.find_element(By.NAME, 'search')
-        search_button = self.driver.find_element(By.CSS_SELECTOR, "button#search_button")
-
-        search_input.send_keys('test post')
-        search_button.click()
-
-        WebDriverWait(self.driver, 10).until(
-            EC.text_to_be_present_in_element((By.XPATH, "//div[@class='card-body']/blockquote/p"), 'This is a test post.')
-        )
-
-        assert 'This is a test post.' in self.driver.page_source
-
-    except Exception as e:
-        print(f"Exception in test_write_post_and_search: {e}")
-        raise
+    def test_write_empty_post(self):
+        try:
+            self.driver.get(self.app_url)
+    
+            name_input = self.driver.find_element(By.NAME, 'name')
+            post_input = self.driver.find_element(By.NAME, 'post')
+    
+            name_input.send_keys('John Doe')
+            post_input.send_keys(Keys.RETURN)
+    
+            WebDriverWait(self.driver, 10).until(
+                EC.text_to_be_present_in_element((By.XPATH, "//div[@class='alert alert-danger']"), 'Message or comment cannot be blank.')
+            )
+            assert 'Message or comment cannot be blank.' in self.driver.page_source
+    
+        except Exception as e:
+            print(f"Exception in test_write_empty_post: {e}")
+            raise
+    
+    def test_write_post_and_comment(self):
+        try:
+            self.driver.get(self.app_url)
+    
+            name_input = self.driver.find_element(By.NAME, 'name')
+            post_input = self.driver.find_element(By.NAME, 'post')
+    
+            name_input.send_keys('John Doe')
+            post_input.send_keys('This is a test post.')
+            post_input.send_keys(Keys.RETURN)
+    
+            WebDriverWait(self.driver, 10).until(
+                EC.text_to_be_present_in_element((By.XPATH, "//div[@class='card-body']/blockquote/p"), 'This is a test post.')
+            )
+    
+            comment_input = self.driver.find_element(By.CSS_SELECTOR, 'input[name="comment"]')
+            comment_input.send_keys('This is a test comment.')
+            comment_input.send_keys(Keys.RETURN)
+    
+            WebDriverWait(self.driver, 10).until(
+                EC.text_to_be_present_in_element((By.XPATH, "//div[@class='card-body']/blockquote/p"), 'This is a test comment.')
+            )
+    
+            assert 'This is a test post' in self.driver.page_source
+            assert 'This is a test comment.' in self.driver.page_source
+    
+        except Exception as e:
+            print(f"Exception in test_write_post_and_comment: {e}")
+            raise
+    
+    def test_write_post_with_category_and_use_filter(self):
+        try:
+            self.driver.get(self.app_url)
+    
+            name_input = self.driver.find_element(By.NAME, 'name')
+            post_input = self.driver.find_element(By.NAME, 'post')
+            categories_checkbox = self.driver.find_element(By.XPATH, "//input[@value='Secrets']")
+            submit_button = self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    
+            name_input.send_keys('John Doe')
+            post_input.send_keys('This is a secret post.')
+            categories_checkbox.click()
+            submit_button.click()
+    
+            WebDriverWait(self.driver, 10).until(
+                EC.text_to_be_present_in_element((By.XPATH, "//div[@class='card-body']/blockquote/p"), 'This is a secret post.')
+            )
+    
+            secrets_filter_button = self.driver.find_element(By.CSS_SELECTOR, "button[value='Secrets']")
+            secrets_filter_button.click()
+    
+            WebDriverWait(self.driver, 10).until(
+                EC.text_to_be_present_in_element((By.XPATH, "//div[@class='card-body']/blockquote/p"), 'This is a secret post.')
+            )
+            assert 'This is a secret post.' in self.driver.page_source
+    
+        except Exception as e:
+            print(f"Exception in test_write_post_with_category_and_use_filter: {e}")
+            raise
+    
+    def test_write_post_and_search(self):
+        try:
+            self.driver.get(self.app_url)
+    
+            name_input = self.driver.find_element(By.NAME, 'name')
+            post_input = self.driver.find_element(By.NAME, 'post')
+            submit_button = self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    
+            name_input.send_keys('John Doe')
+            post_input.send_keys('This is a test post.')
+            submit_button.click()
+    
+            WebDriverWait(self.driver, 10).until(
+                EC.text_to_be_present_in_element((By.XPATH, "//div[@class='card-body']/blockquote/p"), 'This is a test post.')
+            )
+    
+            search_input = self.driver.find_element(By.NAME, 'search')
+            search_button = self.driver.find_element(By.CSS_SELECTOR, "button#search_button")
+    
+            search_input.send_keys('test post')
+            search_button.click()
+    
+            WebDriverWait(self.driver, 10).until(
+                EC.text_to_be_present_in_element((By.XPATH, "//div[@class='card-body']/blockquote/p"), 'This is a test post.')
+            )
+    
+            assert 'This is a test post.' in self.driver.page_source
+    
+        except Exception as e:
+            print(f"Exception in test_write_post_and_search: {e}")
+            raise
 
 if __name__ == '__main__':
     unittest.main()
